db7d6fae1dc4eb8e870613c002d750ae43e1fe9a
ADOUKO Franck Venance <fvadouko@live.fr>
2019-07-13 11:29:06 +0000

    Installation de l'API Platform et mise en place de nos ressources

---
+++ src/Controller/InvoiceIncrementationController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Invoice;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class InvoiceIncrementationController {
+
+    /**
+     * @var ObjectManager
+     */
+    private $manager;
+
+    public function __construct(ObjectManager $objectManager)
+    {
+        $this->manager = $objectManager;
+    }
+
+    public function __invoke(Invoice $data)
+    {
+        $data->setChrono($data->getChrono() + 1);
+        $this->manager->flush();
+        return $data;
+    }
+}

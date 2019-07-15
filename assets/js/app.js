d4578af3559eb0f8cb007aba08f3a092bbcca1b9
ADOUKO Franck Venance <fvadouko@live.fr>
2019-07-15 19:31:42 +0000

    installation et mis en place de react

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.css');
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// const $ = require('jquery');
+
+console.log('Hello');
+
+const App = () => {
+    return <h1> Bonjour à tous !</h1>;
+}
+
+const rootElement = document.querySelector('#app');
+ReactDOM.render(<App />, rootElement);
